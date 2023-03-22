028bfa09fce2633b9ff4709a3dcdbcee16be7f09
Tracy Salloum <tracy.salloum@lau.edu>
2023-03-22 20:48:30 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -3,70 +3,6 @@ import re
 import sys
 import ssl
 
-#SUBDOMAINS
-def subdomains(url):
-
-    #pattern1 is a regex to find the domain of the URL
-    pattern1 = r"^https?\:\/\/([A-Za-z0-9\-]+(?:\.[a-zA-Z]{2,}){1,})$"
-    #pattern2 is a regex to find the protocol of the URL
-    pattern2 = r"https?"
-    #pattern3 is a regex to find the links inside the HTML code
-    pattern3= r"<a[^>]+href=[\"|\']([^\"\']+)[\"|\'][^>]*>"
-    domain = re.findall(pattern1, url)[0]
-    protocol = re.findall(pattern2, url)[0]
-
-    #Reading from the subdomains_dictionary.bat the subdomains to test
-    file = open("subdomains_dictionary.bat")
-    content = file.read()
-    test_subdomains = content.splitlines()
-
-    #Cleaning the string by removing everything not alphanumerical such as white spaces and special characters
-    test_subdomains= [re.sub(r'[^A-Za-z0-9.-_]', '', subdomain) for subdomain in test_subdomains]
-
-    valid_subdomains=[]
-    valid_links=[]
-
-    #Looping until all subdomains are checked
-    for subdomain in test_subdomains:
-        domain_start = url.split("//")[-1].split("/")[0]
-        if domain_start.startswith("www."):
-            #If the domain starts with www.
-            test_url = f"{protocol}://www.{subdomain}.{domain}"
-        else:
-            #If the domain does not start with www.
-            test_url = f"{protocol}://{subdomain}.{domain}"
-        try:
-            #Perform an HTTP GET request to the URL
-            req= requests.get(test_url)
-            if req.status_code >= 200 and req.status_code <= 299:
-                #If valid, add the subdomain to the valid subdomains
-                valid_subdomains.append(subdomain)
-                print("Subdomain found. ", test_url)
-                #Get the HTML code of the valid URL
-                html_content=req.text 
-                #Find all the links inside the HTML code
-                test_links=re.findall(pattern3, html_content)
-                #Loop to test all the links inside the HTML code
-                for l in test_links:
-                    if l.startswith("http"):
-                        req= requests.get(l)
-                        if req.status_code>=200 and req.status_code<=299:
-                            #If the link is valid add it to the valid links array
-                            valid_links.append(l)
-        except Exception as e:
-            print("Subdomain not found. ", test_url)
-
-    #Write the valid subdomains to the output file
-    with open("subdomains_output.bat", "w") as f:
-        f.write("\n".join(valid_subdomains))
-
-    #Write the valid links to the output file
-    with open("links_output.bat", "w") as f:
-        f.write("\n".join(valid_links))
-                
-    file.close()
-
-#DIRECTORIES & FILES
 def directories_files(url):
 
     #pattern1 is a regex to find the domain of the URL
@@ -121,6 +57,74 @@ def directories_files(url):
         f.write("\n".join(valid_links))
         
     file.close()
+
+#SUBDOMAINS
+def subdomains(url):
+
+    #pattern1 is a regex to find the domain of the URL
+    pattern1 = r"^https?\:\/\/([A-Za-z0-9\-]+(?:\.[a-zA-Z]{2,}){1,})$"
+    #pattern2 is a regex to find the protocol of the URL
+    pattern2 = r"https?"
+    #pattern3 is a regex to find the links inside the HTML code
+    pattern3= r"<a[^>]+href=[\"|\']([^\"\']+)[\"|\'][^>]*>"
+    #pattern4 is a regex to find the domain of the URL that starts with www.
+    pattern4= r"^https?\:\/\/www\.([A-Za-z0-9\-]+(?:\.[a-zA-Z]{2,}){1,})$"
+    domain = re.findall(pattern1, url)[0]
+    domain2 = re.findall(pattern4, url)[0]
+    protocol = re.findall(pattern2, url)[0]
+
+    #Reading from the subdomains_dictionary.bat the subdomains to test
+    file = open("subdomains_dictionary.bat")
+    content = file.read()
+    test_subdomains = content.splitlines()
+
+    #Cleaning the string by removing everything not alphanumerical such as white spaces and special characters
+    test_subdomains= [re.sub(r'[^A-Za-z0-9.-_]', '', subdomain) for subdomain in test_subdomains]
+
+    valid_subdomains=[]
+    valid_links=[]
+
+    #Looping until all subdomains are checked
+    for subdomain in test_subdomains:
+        domain_start = url.split("//")[-1].split("/")[0]
+        if domain_start.startswith("www."):
+            #If the domain starts with www.
+            test_url = f"{protocol}://www.{subdomain}.{domain2}"
+        else:
+            #If the domain does not start with www.
+            test_url = f"{protocol}://{subdomain}.{domain}"
+        try:
+            #Perform an HTTP GET request to the URL
+            req= requests.get(test_url)
+            if req.status_code >= 200 and req.status_code <= 299:
+                #If valid, add the subdomain to the valid subdomains
+                valid_subdomains.append(subdomain)
+                print("Subdomain found. ", test_url)
+                #Get the HTML code of the valid URL
+                html_content=req.text 
+                #Find all the links inside the HTML code
+                test_links=re.findall(pattern3, html_content)
+                #Loop to test all the links inside the HTML code
+                for l in test_links:
+                    if l.startswith("http"):
+                        req= requests.get(l)
+                        if req.status_code>=200 and req.status_code<=299:
+                            #If the link is valid add it to the valid links array
+                            valid_links.append(l)
+        except Exception as e:
+            print("Subdomain not found. ", test_url)
+
+    #Write the valid subdomains to the output file
+    with open("subdomains_output.bat", "w") as f:
+        f.write("\n".join(valid_subdomains))
+
+    #Write the valid links to the output file
+    with open("links_output.bat", "w") as f:
+        f.write("\n".join(valid_links))
+                
+    file.close()
+
+#DIRECTORIES & FILES
 
 #Links in the HTML code of the given URL
 def find_links(url):
